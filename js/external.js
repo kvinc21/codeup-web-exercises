25a671c2932b3d11513df5811968b0c9bd329c18
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-05-27 11:48:34 -0500

    codeup-web-exercises

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hello World!");
+console.log("Hello Polaris");
+alert("Hello World!");
+// the following line will show the OK/CANCEL confirm dialog
+var confirmed = confirm('Are you sure you want to do XYZ?');
+console.log(confirmed); // will be either true or false
+var userInput = prompt('Please type something:');
+console.log('The user entered: ' + userInput);
