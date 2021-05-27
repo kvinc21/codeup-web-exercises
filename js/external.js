94e044d06ff64f99df7d3d21e15eb1408e20b1f8
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-05-27 12:15:04 -0500

    codeup-web-exercises

--- js/external.js
+++ js/external.js
@@ -1,9 +1,6 @@
 "use strict";
-console.log("Hello World!");
-console.log("Hello Polaris");
-alert("Hello World!");
-// the following line will show the OK/CANCEL confirm dialog
-var confirmed = confirm('Are you sure you want to do XYZ?');
-console.log(confirmed); // will be either true or false
-var userInput = prompt('Please type something:');
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+var userInput = prompt("What is your favorite color?");
 console.log('The user entered: ' + userInput);
+alert("Great, " + userInput + " is my favorite color too!")
