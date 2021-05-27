aefe01fecd81f832a8f7128809fc881add39c26b
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-05-27 15:00:31 -0500

    codeup-web-exercises

--- js/external.js
+++ js/external.js
@@ -2,5 +2,18 @@
 console.log("Hello from external JavaScript");
 alert("Welcome to my Website!");
 var userInput = prompt("What is your favorite color?");
-console.log('The user entered: ' + userInput);
-alert("Great, " + userInput + " is my favorite color too!")
+console.log("The user entered:" + userInput);
+alert("Great, " + userInput + " is my favorite color too!");
+// Answer for exercise 3
+alert("The price for the movies is: " + (4+5+1))
+
+alert("I am getting paid:" + (400 * 6) + (380 * 4) + (350 * 10))
+
+alert("Is the class full?");
+var full = confirm("")
+var memberquestion = prompt("Are you a premium member");
+if (memberquestion) {
+    alert("")
+}
+alert("")
+
