249bc4bcb94a007465cb0f920b2541e790243555
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-06-04 12:51:39 -0500

    loops ex 1-4

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,7 +1,30 @@
+
 function showMultiplicationTable (x) {
     for (let index = 1; index < 10; index++) {
 
         console.log(x + "x" + index + "=" + (x * index));
     }
 }
-    showMultiplicationTable(7)
+showMultiplicationTable(7);
+
+
+for (let y = 0; y <10; y++) {
+        let random = Math.ceil(Math.random() * 200 - 20) + 20;
+        if (random % 2 !== 0) {
+            console.log(random + " is even");}
+        else{
+            console.log(random + "is odd")
+        }
+}
+
+for (var index = 1, number = ""; index < 10; index++){
+    number +="1";
+    console.log(index * number)
+}
+
+var z = 0;
+for (var z = 100; z >= 5; z = z -5) {
+    console.log(z);
+
+
+}
