60555ddda2a6ec5f5debb6f8228516fecf73115f
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-06-04 10:47:24 -0500

    loops ex 1

---
+++ js/for_loops.js
@@ -0,0 +1,7 @@
+function showMultiplicationTable (x) {
+    for (let index = 1; index < 10; index++) {
+
+        console.log(x + "x" + index + "=" + (x * index));
+    }
+}
+    showMultiplicationTable(7)
