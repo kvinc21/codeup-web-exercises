ffac1fab85cbbd498525a5866bdacabc58b2908d
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-06-07 11:28:00 -0500

    codeup-web-exercises

--- js/do_while.js
+++ js/do_while.js
@@ -1,9 +1,10 @@
 //Do While Loop
-
-var allCones = Math.floor(Math.random()*50) + 50;
-console.log(allCones);
-do {
-    let conesbought = Math.floor(Math.random() * 5) + 1);
-    console.log("")
-
-} while (true);
+//
+// var allCones = Math.floor(Math.random()*50) + 50;
+// console.log(allCones);
+// do {
+//     let conesbought = Math.floor(Math.random() * 5) + 1);
+//     console.log("conesbought")
+//
+// } while ("");
+// }
