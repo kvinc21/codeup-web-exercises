dc244dff9f9bf1c14a50139a66984717119e8090
Kenneth Vincent <kenneth.j.vincent1@gmail.com>
2021-07-30 14:40:41 -0500

    update complete

--- js/promises.js
+++ js/promises.js
@@ -1,2 +1,16 @@
 "use strict"
 
+// function getGithubUsernames() {
+//     return fetch('https://api.github.com/')
+//         .then(response => response.json())
+// }
+//
+// // later on...
+//
+// getGithubUsernames().then( users => {
+//     users.forEach( userObj => {
+//         // do something with each username
+//         console.log(userObj.login);
+//     });
+// }).catch(error => console.error(error));
+
